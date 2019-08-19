993991c0bcbafcd7a46a893433d6bb07b256a911
xieyongjie <xieyongjie@nmrbt.com>
2019-08-19 16:06:04 +0800

    first commit

--- README.md
+++ README.md
@@ -1,29 +1,9 @@
-# for-el-dialog
+# el-dialog-bug
 
-## Project setup
-```
-npm install
-```
+some bug happened after dialog(append-to-body) toggle and v-for data change
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+bug step
+1. click "showDialog" button.
+2. close dialog.
+3. click "changeList" button.
+4. click "changeList" button again.
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>testList: {{testList}}</p>
+    <p v-for="(item, key) in testList" :key="key">{{item}}</p>
+    <el-dialog append-to-body v-show="dialogShow" :visible.sync="dialogShow">contentcontent</el-dialog>
+    <p><el-button type="primary" @click="dialogShow = true">showDialog</el-button></p>
+    <p><el-button @click="changeList">changeList</el-button></p>
+    <p>bug step</p>
+    <p>1. click "showDialog" button.</p>
+    <p>2. close dialog.</p>
+    <p>3. click "changeList" button.</p>
+    <p>4. click "changeList" button again.</p>
+    <p>(it's can be fixed like this)</p>
+    <pre v-pre>&lt;div&gt;&lt;p v-for="(item, key) in testList" :key="key"&gt;{{item}}&lt;/p&gt;&lt;/div&gt;</pre>
+    <p>(or like this)</p>
+    <pre v-pre>&lt;div&gt;&lt;el-dialog append-to-body v-show="dialogShow" :visible.sync="dialogShow"&gt;contentcontent&lt;/el-dialog&gt;&lt;/div&gt;</pre>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      testList: [1, 2, 3, 4, 5, 6],
+      dialogShow: false
+    }
+  },
+  methods: {
+    changeList () {
+      console.log(this.testList = this.testList.length ? [] : [6, 5, 4, 3, 2, 1])
+    }
   }
 }
 </script>
